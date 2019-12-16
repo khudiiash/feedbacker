5f7ac60df72484516a917b01cd7a3af14ba9106a
Dmytro <dmytro.khudiyash@gmail.com>
2019-12-16 11:41:24 +0200

    fix 2

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+const uri = "mongodb+srv://Dmytro:149600earthsun@cluster0-mwooj.mongodb.net/feedback?retryWrites=true&w=majority";
+mongoose.connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
 connection.once('open', () => {
